f7163eb85bf5fe456523f44b2a98c22f15400bf6
jake <jake@vertaxe.com>
2016-06-05 23:34:30 +0200

    fix

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,11 +1,15 @@
 class ReactionsController < ApplicationController
 
   def create
-    if Reaction.create(params)
+    if Reaction.create(reaction_params)
       render json: {success:true}
     else
       render json: {success:false}
     end
+  end
+
+  def reaction_params
+    params.require(:user).permit(:name,:referrer)
   end
 
 end
