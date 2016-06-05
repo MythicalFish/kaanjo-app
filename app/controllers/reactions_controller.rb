22c52cc23de04961e3673bc2a8b46b2df3334c03
jake <jake@vertaxe.com>
2016-06-05 23:36:28 +0200

    fix

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -9,7 +9,7 @@ class ReactionsController < ApplicationController
   end
 
   def reaction_params
-    params.require(:user).permit(:name,:referrer)
+    params.require(:reaction).permit(:name,:referrer)
   end
 
 end
