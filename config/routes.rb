29c9c73194c0af760fd22a4e441fc6ad676855cc
jake <jake@vertaxe.com>
2016-06-05 23:24:30 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
   post '/create-reaction', to: 'reactions#create'
+  mount RailsAdmin::Engine => '/', as: 'rails_admin'
 end
