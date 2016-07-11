3cca4a26fb8c1411a1adbbbe35876d6ec2fff7a2
jake <jake@vertaxe.com>
2016-07-11 17:07:43 +0200

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,23 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception # TODO
 
-  before_filter :add_allow_credentials_headers
+  #before_filter :add_allow_credentials_headers #TODO
 
-  def add_allow_credentials_headers                                                                                                                                                                                                                                                        
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-    response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
-  end 
+  before_action :authenticate_user!
 
-  def options                                                                                                                                                                                                                                                                              
-    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'                                                                                                                                                                                                         
-  end
+  # TODO: not sure if needed
+  #def add_allow_credentials_headers                                                                                                                                                                                                                                                        
+  #  response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+  #  response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+  #end 
+#
+#  #def options                                                                                                                                                                                                                                                                              
+#  #  head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'                                                                                                                                                                                                         
+  #end
+
+  private
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   post '/create-reaction', to: 'reactions#create'
   mount RailsAdmin::Engine => '/', as: 'rails_admin'
 end
