e7d384be7c23958843c4a4a0209cf93338ebd3a0
jake <jake@vertaxe.com>
2016-07-12 16:46:20 +0200

    products query done

--- app/models/webmaster.rb
+++ app/models/webmaster.rb
@@ -11,22 +11,35 @@ class Webmaster < User
   def products(
 
   from: Time.new(1970), 
-  to: Time.now, 
+  to: Time.now.end_of_day, 
   order_by: 'count(*)', 
   order_direction: 'DESC', 
   reaction_type_ids: ReactionType.ids )
 
 
-  reactions.
-    joins(:reaction_type).
-    where('
-      reactions.created_at BETWEEN ? AND ? and
-      reaction_types.id IN (?)',
-      from,to,
-      reaction_type_ids
-    ).
-    group('product').
-    order("#{order_by} #{order_direction}")
+    products = reactions.
+      where('
+        created_at BETWEEN ? AND ? and
+        reaction_type_id IN (?)',
+        from,to,
+        reaction_type_ids
+      ).
+      group(['product','reaction_type_id']).
+      order("#{order_by} #{order_direction}").
+      count
+
+    results = {}
+
+    products.each do |p,count|
+      product = p[0]
+      reaction_type = ReactionType.find(p[1]).name
+      results[product] ||= {}
+      results[product][reaction_type] = count
+      results[product]['Total'] ||= 0
+      results[product]['Total'] += count
+    end
+
+    results
 
   end
 
