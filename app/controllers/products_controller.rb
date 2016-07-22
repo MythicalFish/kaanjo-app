6763fcf3bbc9057f7a4980b5f4b138950a5bb1d7
jake <jake@vertaxe.com>
2016-07-22 18:26:26 +0200

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,27 @@ class ProductsController < ApplicationController
 
   def index
     @title = "Products"
-    @products = current_webmaster.products.includes([:impressions,:reactions])
+    @products = current_webmaster.products.with_counts(args)
   end
 
   def show
     @product = Product.find(params[:id])
     @title = @product.name
+  end
+
+  private
+
+  def args
+    args = {
+      from: from_date,
+      to: to_date
+    }
+
+    args[:order] = params[:a] if params[:a]
+    args[:direction] = params[:d] if params[:d]
+    args[:direction] = 'ASC' if args[:direction] == 'up'
+    args[:direction] = 'DESC' if args[:direction] == 'down'
+    args
   end
 
 end
