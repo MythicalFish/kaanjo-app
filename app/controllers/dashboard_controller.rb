6406154c139bcf88d90799c1f51be4681f14b546
jake <jake@vertaxe.com>
2016-08-11 18:07:55 +0200

    test page & dashboard updates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,15 +2,20 @@ class DashboardController < ApplicationController
 
   def show
     @title = 'Dashboard'
+    f = from_date
+    t = to_date
     if admin?
+      @impression_total = Impression.where(created_at:f..t).length
+      @reaction_total =   Reaction.where(created_at:f..t).length
+      @webmasters =  Webmaster.all
+      @total_ctr = calculate_ctr(@reaction_total, @impression_total).to_s << '%'
       render 'dashboard/for_admin' and return
     elsif webmaster?
       w = current_webmaster
-      f = from_date
-      t = to_date
       @impression_total = w.impressions.total f,t
       @reaction_total =   w.reactions.total   f,t
       @reaction_counts =  w.reactions.counts  f,t
+      @total_ctr = calculate_ctr(@reaction_total, @impression_total).to_s << '%'
       @products =  w.products.top_by_type     f,t
       render 'dashboard/for_webmaster'
     else
--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -4,12 +4,16 @@ module DataHelper
     if object.respond_to?(:total_ctr)
       return "#{object.total_ctr}%"
     elsif object.respond_to?(:impression_count) && object.respond_to?(:reaction_total)
-      ctr = ((object.reaction_total.to_f / object.impression_count.to_f) * 100).round(3) 
+      ctr = calculate_ctr(object.reaction_total, object.impression_count)
       ctr = 0 unless ctr > 0
       return "#{ctr}%"
     else 
       return '?'
     end
+  end
+
+  def calculate_ctr reactions, impressions
+    ((reactions.to_f / impressions.to_f) * 100).round(2) 
   end
 
 end
