9637239da2498106e80344c297cc29c7661eab00
jake <jake@vertaxe.com>
2016-08-16 09:47:37 +0200

    show login at root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user_custom
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   include ApplicationHelper
@@ -18,18 +18,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: custom_attributes)
   end
 
-
-  #before_filter :add_allow_credentials_headers #TODO
-
-  # TODO: not sure if needed
-  #def add_allow_credentials_headers                                                                                                                                                                                                                                                        
-  #  response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-  #  response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
-  #end 
-#
-#  #def options                                                                                                                                                                                                                                                                              
-#  #  head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'                                                                                                                                                                                                         
-  #end
-
+  def authenticate_user_custom
+    authenticate_user! unless request.path == "/"
+  end
 
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,11 +5,7 @@ class DashboardController < ApplicationController
     f = from_date
     t = to_date
     if admin?
-      @impression_total = Impression.where(created_at:f..t).length
-      @reaction_total =   Reaction.where(created_at:f..t).length
-      @webmasters =  Webmaster.all
-      @total_ctr = calculate_ctr(@reaction_total, @impression_total).to_s << '%'
-      render 'dashboard/for_admin' and return
+      dashboard_for_admin and return
     elsif webmaster?
       w = current_webmaster
       @impression_total = w.impressions.total f,t
@@ -19,8 +15,16 @@ class DashboardController < ApplicationController
       @products =  w.products.top_by_type     f,t
       render 'dashboard/for_webmaster'
     else
-      raise "Neither admin nor webmaster"
+      render 'devise/sessions/new', locals: { resource: User.new }
     end
+  end
+
+  def dashboard_for_admin
+    @impression_total = Impression.where(created_at:from_date..to_date).length
+    @reaction_total =   Reaction.where(created_at:from_date..to_date).length
+    @webmasters =  Webmaster.all
+    @total_ctr = calculate_ctr(@reaction_total, @impression_total).to_s << '%'
+    render 'dashboard/for_admin'
   end
 
 end
