fdd232123264921a8068c078d3f825a0c6854ac2
jake <jake@vertaxe.com>
2016-11-10 21:22:10 +0700

    chart styling

--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -7,9 +7,13 @@ module ChartHelper
     dates = []
     values = []
 
+    aqua = '#0EA0EB'
+    gray = '#E9EDEF'
+
     c.each do |date,value|
       dates << date
-      values << value[property]
+      v = value[property]
+      values << v
     end
 
     {
@@ -19,23 +23,14 @@ module ChartHelper
         datasets: [{
           label: label,
           data: values,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          backgroundColor: 'transparent',
+          borderColor: gray,
+          borderWidth: 4,
+          pointBorderColor: aqua,
+          pointBackgroundColor: aqua,
+          pointHoverBackgroundColor: '#F1F5F8',
+          pointRadius: 6,
+          lineTension: 0
         }]
     },
     options: {
@@ -43,8 +38,12 @@ module ChartHelper
         yAxes: [{
             ticks: {
               beginAtZero:true
-            }
+            },
+            scaleLabel: 'function(v) { return v+"%"; }'
           }]
+        },
+        tooltips: {
+          backgroundColor: aqua
         }
       }
     }
