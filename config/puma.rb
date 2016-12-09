510dfaf90643a999f8f478cfebafe32d82e3218a
jake <jake@vertaxe.com>
2016-12-09 14:13:39 +0700

    disable cluster mode

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 # Change to match your CPU core count
-workers 1
+#workers 1 # default 0
 
 # Min and Max threads per worker
-threads 1, 6
+#threads 1, 16 # default 1,16
 
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
